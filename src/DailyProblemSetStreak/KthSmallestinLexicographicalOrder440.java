a4d803f2cf56272ccde642d8b94fc56c1acded35
Dheeraj Kumar <adj1918.d@gmail.com>
2025-06-10 00:13:18 +0530

    Day 22

--- src/DailyProblemSetStreak/KthSmallestinLexicographicalOrder440.java
+++ src/DailyProblemSetStreak/KthSmallestinLexicographicalOrder440.java
@@ -1,10 +1,40 @@
 package DailyProblemSetStreak;
 
+import javax.swing.plaf.metal.MetalTheme;
 import java.util.*;
 
 public class KthSmallestinLexicographicalOrder440 {
+
+    private int count(int current, int next, int n) {
+        int countNumber = 0;
+        while(current <= n){
+            countNumber += (next-current);
+            current *= 10;
+            next = Math.min(next*10 ,n+1);
+        }
+        return countNumber;
+    }
+
     public int findKthNumber(int n, int k) {
 
+        int current = 1;
+        k -= 1; // since one  is already considered.
+
+        while(k>0){
+            int numberCount = count(current,current+1,n);
+
+            if(numberCount <= k){
+                current += 1;
+                k -= numberCount; // skip the number occured on that branch before jumping to the next branch.
+            } else{
+                // so we need to go dipper in the tree and will consider one node in our count.
+                current *= 10; // to go one leve deep
+                k -= 1; // now consider that node and subtract the  from the k.
+            }
+        }
+        return  current;
+
+        /* It will give TLE
         List<Integer>array = new ArrayList<>();
 
         for(int i = 1;i<=n;i++){
@@ -17,7 +47,10 @@ public class KthSmallestinLexicographicalOrder440 {
             }
         });
         return array.get(k-1);
+        */
     }
+
+
 
     public static void main(String [] args){
         KthSmallestinLexicographicalOrder440 solver = new KthSmallestinLexicographicalOrder440();
