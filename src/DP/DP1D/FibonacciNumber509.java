8614ff19855b6ffa27da936fcdd792c6132c8803
Dheeraj Kumar <adj1918.d@gmail.com>
2025-06-21 16:17:54 +0530

    BottomsUp approch for fib

--- src/DP/DP1D/FibonacciNumber509.java
+++ src/DP/DP1D/FibonacciNumber509.java
@@ -6,11 +6,32 @@ public class FibonacciNumber509 {
     public int fib(int n) {
         if(n == 0 || n == 1) return n;
         int[] dp = new int [n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        /**
+         * now let's try bottoms up approach where we already have value.
+         * let's define the state here.
+         * dp[0] = 0, dp[1] = 1
+         * dp[i] will contains the calculated fibValue for the i th index.
+         * dp[2] = dp[1] + dp[0] => 1+0= 1;
+         * dp[3] = dp[2] + dp[1] => 1+1 = 2;
+         * dp[4] = dp[3] + dp[2] => 2+1 = 3;
+         * dp[5] = dp[4] + dp[3] => 3+2 = 5;
+         */
+
+        for(int i = 2 ;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+        /*
+        so here we are using the recursion and saving the result in on memo. to resue it
+        */
+        /*
         Arrays.fill(dp,-1);
         dp[0] = 0;
         dp[1] = 1;
         return findFibUsingRecursionTopToDownApproch(n, dp);
-        //return dp[n];
+        */
     }
 
     private int findFibUsingRecursionTopToDownApproch(int n, int[] dp) {
