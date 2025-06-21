e734f64d063d78ffdf8b3400f7cf3dd2942b2822
Dheeraj Kumar <adj1918.d@gmail.com>
2025-06-21 20:31:53 +0530

    Dp for house Robber

--- src/DP/DP1D/HouseRobber198.java
+++ src/DP/DP1D/HouseRobber198.java
@@ -1,10 +1,12 @@
 package DP.DP1D;
 
+import java.util.Arrays;
+
 public class HouseRobber198 {
     public int rob(int[] nums) {
         /**
          * So here we have a robber, he can't rob two adjacent house.
-         * so he will have two choice either to rob the house or skip the house.
+         * so he will have two choice either to rob the house and keep the profit or skip the house.
          * so if he robbed a house then he must need to leave the just next house
          * he can have option next to that.
          */
@@ -12,15 +14,18 @@ public class HouseRobber198 {
          * let's try using Brute force approch.
          */
         int i = 1;
-        return robByRecursion(i,nums);
+        int [] dp = new int [nums.length+1];
+        Arrays.fill(dp,-1);
+        return robByRecursion(i,nums,dp);
 
     }
-
-    private int robByRecursion(int i, int[] nums) {
+    private int robByRecursion(int i, int[] nums,int[]dp) {
         if(i > nums.length) return 0;
-        int steal = robByRecursion(i + 2,nums) + nums[i-1];
-        int skip = robByRecursion(i + 1, nums);
-        return Math.max(steal,skip);
+        if(dp[i] != -1) return dp[i];
+        int steal = robByRecursion(i + 2,nums,dp) + nums[i-1];
+        int skip = robByRecursion(i + 1, nums,dp);
+        dp[i] = Math.max(steal,skip);
+        return dp[i];
     }
 
     public static void main(String[] args) {
