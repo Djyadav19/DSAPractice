154b861822c7741482e4d0cf8734e644321ca3ad
Dheeraj Kumar <adj1918.d@gmail.com>
2025-06-23 21:52:41 +0530

    Leetcode 75

---
+++ src/LeetCode75/DP1D/NthTribonacciNumber1137.java
@@ -0,0 +1,40 @@
+package LeetCode75.DP1D;
+
+import java.util.Arrays;
+
+public class NthTribonacciNumber1137 {
+    public int tribonacci(int n) {
+
+        /**
+         let's start with recursion and memoization.
+
+         */
+        if( n == 0)return 0;
+        if(n < 3) return 1;
+        int [] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        return calculateTribonacci(n, dp);
+
+    }
+    private int calculateTribonacci(int n, int[]dp){
+        if(n == 0 )return 0;
+        if(n == 1|| n == 2) return 1;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = calculateTribonacci(n-3,dp) + calculateTribonacci(n-2,dp) + calculateTribonacci(n-1,dp);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        NthTribonacciNumber1137 solver = new NthTribonacciNumber1137();
+        System.out.println(solver.tribonacci(0)); // Output: 0
+        System.out.println(solver.tribonacci(1)); // Output: 1
+        System.out.println(solver.tribonacci(2)); // Output: 1
+        System.out.println(solver.tribonacci(3)); // Output: 2
+        System.out.println(solver.tribonacci(4)); // Output: 4
+        System.out.println(solver.tribonacci(25)); // Output: 1389537
+    }
+}
