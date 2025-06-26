f57d1c54441515599076f6df491bd81ef6819bbe
Dheeraj Kumar <adj1918.d@gmail.com>
2025-06-26 23:46:10 +0530

    Day 39 most optimal way

--- src/DailyProblemSetStreak/LongestBinarySubsequenceLessThanorEqualtoK2311.java
+++ src/DailyProblemSetStreak/LongestBinarySubsequenceLessThanorEqualtoK2311.java
@@ -5,15 +5,44 @@ import java.util.Arrays;
 public class LongestBinarySubsequenceLessThanorEqualtoK2311 {
     public int longestSubsequence(String s, int k) {
 
+
+        /**
+         * now let's try the most optimal way of o(n).
+         * Here let's construct the binary number .
+         * we can see that adding 0 is not going to impact as we are iterating from n-> 0
+         * only the value of binary is going to increase when we take 1.
+         * for each one it will power of pow(2,index).
+         * so if we take 1 we need to subtract that much value from the k.
+         * if we not take we must need to move ahead, but we always need to update the value as it will
+         * increase by lastvalue * 2.
+         */
+        int n = s.length();
+        int valueOf1AtThatIndex = 1 ; // pow of 2^0.
+        int length = 0;
+
+        for(int i = n-1;i>=0;i--){
+            if(s.charAt(i) == '0'){
+                length++; // since adding zero in front of the number is not going to impact the value.
+            } else if(valueOf1AtThatIndex <= k){
+                k -= valueOf1AtThatIndex;
+                length++;
+            }
+            if(valueOf1AtThatIndex <= k){
+                valueOf1AtThatIndex <<= 1;
+            }
+        }
+        return length;
         /**
          * here we are using recursion and memoization.
          * using the dp[] array. .
          */
+        /**
         int n = s.length();
         int []dp = new int[n];
         Arrays.fill(dp,-1);
         //dp[n-1] = 0;
         return solveUsingRecurssion(s,n,n-1,k,dp);
+         */
     }
 
     private int solveUsingRecurssion(String s, int n, int i, int k, int[] dp){
