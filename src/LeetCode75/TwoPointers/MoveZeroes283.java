960d59ac8c85d5f16e6e1f64a4c883259a926ca5
Dheeraj Kumar <adj1918.d@gmail.com>
2025-06-29 23:00:43 +0530

    Leet code 75

---
+++ src/LeetCode75/TwoPointers/MoveZeroes283.java
@@ -0,0 +1,60 @@
+package LeetCode75.TwoPointers;
+
+public class MoveZeroes283 {
+
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 1;
+        while(left<nums.length && right < nums.length){
+            if(nums[left] != 0){
+                left++;
+                if(left == right){
+                    right++;
+                }
+                continue;
+            }
+            if(nums[left] == 0 && nums[right] != 0){
+                nums[left] = nums[right];
+                nums[right] = 0;
+                right++;
+            } else if(nums[left] == 0 && nums[right] == 0){
+                while(right < nums.length){
+                    if(nums[right] != 0){
+                        nums[left] = nums[right];
+                        nums[right] = 0;
+                        break;
+                    }
+                    right++;
+                }
+            }
+            left++;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes283 solution = new MoveZeroes283();
+
+        int[][] testCases = {
+            {0,1,0,3,12},
+            {0,0,1},
+            {1,0,1},
+            {0,0,0},
+            {1,2,3,4,5},
+            {0},
+            {},
+            {2,0,0,1,3,0,4}
+        };
+
+        for (int[] testCase : testCases) {
+            int[] nums = testCase.clone();
+            solution.moveZeroes(nums);
+            System.out.print("Result: ");
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
