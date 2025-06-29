7cda2a3b5370b178654e114dcc8974410a3fa6db
Dheeraj Kumar <adj1918.d@gmail.com>
2025-06-29 23:08:09 +0530

    Leet code 75 with a better way

--- src/LeetCode75/TwoPointers/MoveZeroes283.java
+++ src/LeetCode75/TwoPointers/MoveZeroes283.java
@@ -3,6 +3,23 @@ package LeetCode75.TwoPointers;
 public class MoveZeroes283 {
 
     public void moveZeroes(int[] nums) {
+        /**
+         * let's start here with the intuition that we will first fill all the non zeros in start
+         * and the will fill the remaining index with 0.
+          */
+
+        int pos = 0;
+        for(int num : nums){
+            if(num != 0){
+                nums[pos++] = num;
+            }
+        }
+        while(pos < nums.length){
+            nums[pos++] = 0;
+        }
+
+        /**
+        //naive
         int left = 0;
         int right = 1;
         while(left<nums.length && right < nums.length){
@@ -29,8 +46,7 @@ public class MoveZeroes283 {
             }
             left++;
         }
-
-
+         */
     }
 
     public static void main(String[] args) {
