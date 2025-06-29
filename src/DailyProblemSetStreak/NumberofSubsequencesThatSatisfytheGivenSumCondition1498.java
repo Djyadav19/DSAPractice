b34a6adf715afee69870e59bd6c6bf860852d7c7
Dheeraj Kumar <adj1918.d@gmail.com>
2025-06-29 15:01:10 +0530

    Day 42

--- src/DailyProblemSetStreak/NumberofSubsequencesThatSatisfytheGivenSumCondition1498.java
+++ src/DailyProblemSetStreak/NumberofSubsequencesThatSatisfytheGivenSumCondition1498.java
@@ -1,11 +1,56 @@
 package DailyProblemSetStreak;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class NumberofSubsequencesThatSatisfytheGivenSumCondition1498 {
 
+    public int numSubseq(int[] nums, int target) {
+        /**
+         * so here the main thought process is to think that sorting will not going to change the answer as we have take only max and min
+         * so sorting is not a problem.
+         * now the second thought is that we always have two choices either to take or not that make the for each field has 2 choiece.
+         * which lead that if we have 5 place to take then 2*2*2*2*2.
+         * so we will calculate the value at start only.
+         */
+        int n = nums.length;
+        int MOD = 1_000_000_007;
+        Arrays.sort(nums);
+        int []pow = new int[n];
+        pow[0] = 1;
+        //pre calculate the power
+        for(int i = 1;i<n;i++){
+            pow[i] = (pow[i-1] * 2)%MOD;
+        }
+        int answer = 0;
+        int left = 0;
+        int right = n-1;
+        while(left<=right){
+            if(nums[left]+nums[right]<=target){
+                answer = (answer+pow[right-left])%MOD;
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return answer;
 
+
+        /**
+         lets' start with take skip approch and see .
+         */
+        /*
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        Map<String, Integer> memo = new HashMap<>();
+        return count(nums, 0, n, Integer.MIN_VALUE, Integer.MAX_VALUE, target, false, memo);*/
+        // return answer;
+    }
+
+    /*
     private static final int MOD = 1_000_000_007;
     private int count(int [] nums, int curr, int n, int max, int min, int target, boolean taken, Map<String, Integer> memo){
         String key = curr + "," + max + "," + min + "," + taken;
@@ -25,19 +70,7 @@ public class NumberofSubsequencesThatSatisfytheGivenSumCondition1498 {
         memo.put(key,res);
         return res;
     }
-
-    public int numSubseq(int[] nums, int target) {
-        /**
-         lets' start with take skip approch and see .
-         */
-        int n = nums.length;
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int start = 0;
-        Map<String, Integer> memo = new HashMap<>();
-        return count(nums, 0, n, Integer.MIN_VALUE, Integer.MAX_VALUE, target, false, memo);
-        // return answer;
-    }
+     */
 
     public static void main(String[] args) {
         NumberofSubsequencesThatSatisfytheGivenSumCondition1498 solver = new NumberofSubsequencesThatSatisfytheGivenSumCondition1498();
