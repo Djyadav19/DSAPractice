d1b30cb790a3f3c0cca23b8a8cfd788e0c7bef42
Dheeraj Kumar <adj1918.d@gmail.com>
2025-07-03 10:17:21 +0530

    Day 46

---
+++ src/DailyProblemSetStreak/FindtheKthCharacterinStringGameI3304.java
@@ -0,0 +1,26 @@
+package DailyProblemSetStreak;
+
+public class FindtheKthCharacterinStringGameI3304 {
+    public char kthCharacter(int k) {
+        String startWord = "a";
+        while(startWord.length() < k){
+            StringBuilder newTemp = new StringBuilder();
+            for(int i = 0;i<startWord.length();i++){
+                newTemp.append((char)(startWord.charAt(i) + 1));
+            }
+            startWord = startWord + newTemp.toString();
+        }
+        return startWord.charAt(k-1);
+    }
+
+    public static void main(String[] args) {
+        FindtheKthCharacterinStringGameI3304 solver = new FindtheKthCharacterinStringGameI3304();
+        System.out.println(solver.kthCharacter(1));   // Expected: a
+        System.out.println(solver.kthCharacter(2));   // Expected: b
+        System.out.println(solver.kthCharacter(3));   // Expected: b
+        System.out.println(solver.kthCharacter(4));   // Expected: c
+        System.out.println(solver.kthCharacter(10));  // Example test
+        System.out.println(solver.kthCharacter(500)); // Edge test
+    }
+
+}
