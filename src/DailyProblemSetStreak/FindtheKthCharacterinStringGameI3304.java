568131d25afda3fdee3ef35aefdc3f39633afa3b
Dheeraj Kumar <adj1918.d@gmail.com>
2025-07-04 22:31:13 +0530

    Better approach

--- src/DailyProblemSetStreak/FindtheKthCharacterinStringGameI3304.java
+++ src/DailyProblemSetStreak/FindtheKthCharacterinStringGameI3304.java
@@ -2,17 +2,22 @@ package DailyProblemSetStreak;
 
 public class FindtheKthCharacterinStringGameI3304 {
     public char kthCharacter(int k) {
-        String startWord = "a";
+        //count the bit.
+        int bitCount = Integer.bitCount(k-1);
+        return (char) ('a'+bitCount);
+
+       /** String startWord = "a";
         int count = 1;
         while(startWord.length() < k){
             StringBuilder newTemp = new StringBuilder();
             count = startWord.length();
             for(int i = 0;i<startWord.length() && k > count+i;i++){
-                newTemp.append((char)(startWord.charAt(i) + 1));
+                newTemp.append((char)(startWord.charAt(i) == 'z'?'a':startWord.charAt(i) + 1));
             }
             startWord = startWord + newTemp.toString();
         }
         return startWord.charAt(k-1);
+        */
     }
 
     public static void main(String[] args) {
