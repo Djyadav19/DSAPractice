41c11aac546579988fd43b45ae7363ba80e9d9ad
Dheeraj Kumar <adj1918.d@gmail.com>
2025-07-08 22:23:58 +0530

    Day 51: there it require binarySearch to find the next one instead of iteration for each and HashMap as memo to save space

--- src/DailyProblemSetStreak/MaximumNumberofEventsThatCanBeAttendedII1751.java
+++ src/DailyProblemSetStreak/MaximumNumberofEventsThatCanBeAttendedII1751.java
@@ -1,31 +1,45 @@
 package DailyProblemSetStreak;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MaximumNumberofEventsThatCanBeAttendedII1751 {
 
-    private int solve(int curr, int prev, int[][] events, int k, int[][][] dp) {
-        if (curr >= events.length || k == 0) return 0;
-        if (dp[curr][prev + 1][k] != -1) return dp[curr][prev + 1][k];
-
-        int take = 0;
-        if (prev == -1 || events[prev][1] < events[curr][0]) {
-            take = events[curr][2] + solve(curr + 1, curr, events, k - 1, dp);
+    private int bs(int[][]events,int curr){
+        int l = curr+1;
+        int r = events.length-1;
+        int res = events.length; // if didn't find any then will return the end of the events
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(events[mid][0] > events[curr][1]){
+                res = mid;
+                r = mid -1;
+            }else{
+                l = mid+1;
+            }
         }
-        int skip = solve(curr + 1, prev, events, k, dp);
+        return res;
+    }
 
-        dp[curr][prev + 1][k] = Math.max(take, skip);
-        return dp[curr][prev + 1][k];
+    private int solve(int curr, int[][] events, int k, Map<String, Integer> memo) {
+        if (curr >= events.length || k == 0) return 0;
+        String key = curr + "," + k;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        int next = bs(events,curr);
+        int take =events[curr][2] + solve(next, events, k - 1, memo);
+        int skip = solve(curr + 1, events, k, memo);
+
+        int res = Math.max(take, skip);
+        memo.put(key, res);
+        return res;
     }
 
     public int maxValue(int[][] events, int k) {
         Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        int n = events.length;
-        int[][][] dp = new int[n][n + 1][k + 1];
-        for (int[][] arr2d : dp)
-            for (int[] arr1d : arr2d)
-                Arrays.fill(arr1d, -1);
-        return solve(0, -1, events, k, dp);
+        Map<String, Integer> memo = new HashMap<>();
+        return solve(0,events, k, memo);
     }
 
     public static void main(String[] args) {
